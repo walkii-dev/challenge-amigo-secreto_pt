2cf9192cd0324f7103857b3da8ac42737a6119cd
Lucas Oliveira <lucasp.oliveira822@gmail.com>
2025-02-18 09:45:48 -0300

    adicionando amigos no array

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
 
+function adicionarAmigo() {
+    let novoAmigo = document.querySelector('input').value;
+    amigos.push(novoAmigo);
+    console.log(amigos);
+    
+    
+    if (novoAmigo == ''){
+        alert('Por favor,insira um nome válido.');
+    }
+
+
+}
