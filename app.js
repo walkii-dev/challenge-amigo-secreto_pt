267dd4488d0858dbdf98fc0fb10fb02409c816ce
Lucas Oliveira <lucasp.oliveira822@gmail.com>
2025-02-20 11:41:14 -0300

    finalizado a adição de amigos na array, ja validado

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
 
 function adicionarAmigo() {
     let novoAmigo = document.querySelector('input').value;
-    amigos.push(novoAmigo);
-    console.log(amigos);
-    
-    
-    if (novoAmigo == ''){
-        alert('Por favor,insira um nome válido.');
+
+    if(novoAmigo == ''){
+        alert('Por favor, insira um nome válido.'); //poderia ter sido encorporado ao texto acima do campo. além de uma validação de não ser adicionado número.
+    } else {
+        amigos.push(novoAmigo);
     }
 
+    console.log(amigos);
+    
+    limpaCampo(); // no início, pensei que poderia jogar o código dela dentro da função, mas não é legal repetir código.
+    alteraTexto('insira o nome do próximo amigo.'); //não era obrigatório no projeto, mas fiz mesmo assim ^^
+}
 
+function limpaCampo(){
+    let limpa = document.querySelector('input');
+    limpa.value = '';
+}
+
+function alteraTexto(texto){
+    let mensagem = document.querySelector('h2');
+    mensagem.innerHTML = texto;
 }
