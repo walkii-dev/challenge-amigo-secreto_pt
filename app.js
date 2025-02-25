d25a56825ebe9957431df0f66cac22d65ddd3428
Lucas Oliveira <lucasp.oliveira822@gmail.com>
2025-02-25 09:11:33 -0300

    função que cria um amigo na lista

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ function adicionarAmigo() {
         alert('Por favor, insira um nome válido.'); //poderia ter sido encorporado ao texto acima do campo. além de uma validação de não ser adicionado número.
     } else {
         amigos.push(novoAmigo);
+        amigosNaLista();
     }
 
-    console.log(amigos);
+    
     
     limpaCampo(); // no início, pensei que poderia jogar o código dela dentro da função, mas não é legal repetir código.
     alteraTexto('insira o nome do próximo amigo.'); //não era obrigatório no projeto, mas fiz mesmo assim ^^
@@ -23,4 +24,19 @@ function limpaCampo(){
 function alteraTexto(texto){
     let mensagem = document.querySelector('h2');
     mensagem.innerHTML = texto;
+}
+
+function amigosNaLista() {
+
+    let lista = document.getElementById('listaAmigos'); // pega a referencia na lista pelo id HTML
+
+    lista.innerHTML = ''; //define como nada pra ela começar zerada
+
+    for (let i = 0; i < amigos.length ; i++ ){ // uma forma de acessar cada elemento da lista 
+        let li = document.createElement('li');  //li vai receber uma criação de  tag li
+        li.textContent = amigos[i]; //como está dentro do laço for, ela vai fazer um ciclo, a cada ciclo, vai 'guardar' o nome escrito no Li
+        lista.appendChild(li); //isso é uma referencia que está ligado a lista ordenada.
+
+    }
+
 }
